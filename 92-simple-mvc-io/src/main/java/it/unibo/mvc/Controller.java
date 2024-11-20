f34b25349bf930d7957984e1ba463b4f5b2f3387
Wanes01 <emir.wanes.aouioua@gmail.com>
2024-11-20 16:43:22 +0100

    92: Controller completed

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,37 @@
 package it.unibo.mvc;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+
+    private static final String SEP = File.separator;
+    private static final String DEFAULT_FILE_NAME = "output.txt";
+    private static final String DEFAULT_FILE_PATH = System.getProperty("user.home") + SEP + DEFAULT_FILE_NAME;
+    private File currFile = new File(DEFAULT_FILE_PATH);
+
+    public void setFile(File file) {
+        this.currFile = file;
+    }
+
+    public File getFile() {
+        return this.currFile;
+    }
+
+    public String getFilePath() {
+        return this.currFile.getPath();
+    }
+
+    public void saveOnFile(String text) throws IOException {
+        try (final BufferedWriter fw = new BufferedWriter(
+                new FileWriter(this.getFilePath()))) {
+            fw.write(text);
+        }
+    }
 
 }
